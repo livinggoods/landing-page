533a03f6d5a526b4fdeb19bd9a679707aa9c6a7d
Ong3r1 <Ong3ri@gmail.com>
2017-08-18 17:32:09 +0300

    updated backend to return more than one link from the database

--- php/front-end_endpoint.php
+++ php/front-end_endpoint.php
@@ -5,18 +5,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
 	if (isset($_GET['data'])) {
 		if ($_GET['data'] == 'categories') {
-			
-			$category_array = array();
 
 			$query = "SELECT * FROM categories;";
-
 			if($result = $conn -> query($query)){
+				$category_array = array();
 				while ($row = $result->fetch_assoc()) {
-					$id = $row['id'];
+					$category_id = $row['id'];
 					$name = $row['name'];
-					$category_array[$id]['id'] = $id;
-					$category_array[$id]['name'] = $name;
+					$category_array[$category_id]['id'] = $category_id;
+					$category_array[$category_id]['name'] = $name;
 				}
+			}
+
+			$links_array = array();
+			$link_query = "SELECT * FROM links;";
+			if ($result = $conn -> query($link_query)) {
+
+				while ($lnkrow = $result->fetch_assoc()) {
+					$link_id = $lnkrow['id'];
+					$link_url = $lnkrow['url'];
+					if (in_array($link_id, $links_array)) {
+						$links_array[$link_id]['count']++;
+						array_push($links_array[$link_id]['urls'], $link_url);
+					}else{
+						array_push($links_array, $link_id);
+						$links = array();
+						$links['count'] = 1;
+						$links['urls'] = array();
+
+						array_push($links['urls'], $link_url);
+						$links_array[$link_id] = $links;
+					}
+				}
+			}else{
+				$Error = array('SQL ERROR: ' => mysqli_error($conn));
+				echo json_encode($Error);
 			}
 
 			$query = "SELECT * FROM application;";
@@ -26,35 +49,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 				while ($row = $result->fetch_assoc()) {
 					$category = $row['category'];
 					$category_name = $category_array[$category]['name'];
-					$name = $row['name'];
-					$image = $row['image'];
-					$link = $row['link'];
-					$description = $row['description'];
-					if (in_array($category, $category_array)) {
-						$data_array[$category]['name'] = $category_name;
-						$app['name'] = $name;
-						$app['image'] = $image;
-						$app['link'] = $link;
-						$app['description'] = $description;
+					$app_name = $row['name'];
+					$app_image = $row['image'];
+					$app_description = $row['description'];
+					$app_id = $row['id'];
+					if (array_key_exists($category_name, $data_array)) {
+						if (!array_key_exists($app_name, $data_array[$category_name]['apps'])) {
+							$tmp_app['name'] = $app_name;
+							$tmp_app['image'] = $app_image;
+							$tmp_app['links'] = $links_array[$app_id];
+							$tmp_app['description'] = $app_description;
 
-						array_push($data_array[$category]['apps'], $app);
+							$data_array[$category_name]['apps'][$app_name] = $tmp_app;
+						}
+						echo "in array";
 					}else{
-						array_push($category_array, $row['category']);
-						$data_array[$category]['name'] = $category_name;
-						$data_array[$category]['apps'] = array();
-						$app['name'] = $name;
-						$app['image'] = $image;
-						$app['link'] = $link;
-						$app['description'] = $description;
+						$tmp_app['name'] = $app_name;
+						$tmp_app['image'] = $app_image;
+						$tmp_app['links'] = $links_array[$app_id];
+						$tmp_app['description'] = $app_description;
 
-						array_push($data_array[$category]['apps'], $app);
+						array_push($category_name, $data_array);
+						$data_array[$category_name]['name'] = $category_name;
+						$data_array[$category_name]['apps'] = array();
+
+						$data_array[$category_name]['apps'][$app_name] = $tmp_app;
 					}
 				}
-				echo json_encode($data_array);
+				echo json_encode(array("categories"=>$data_array));
 			}else{
 				$Error = array('SQL ERROR: ' => mysqli_error($conn));
 				echo json_encode($Error);
 			}
+
+			
 		}else{
 			echo "no data requested";
 		}
