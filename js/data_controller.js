159836a4b05721219e2c82c77093398e4511d4be
Ong3r1 <Ong3ri@gmail.com>
2017-08-31 11:31:54 +0300

    fixed untracked files

--- js/data_controller.js
+++ js/data_controller.js
@@ -8,36 +8,38 @@ var mainApp;
 mainApp = angular.module('mainApp', []);
 
 mainApp.controller('mainController', ['$scope', '$window', '$http', function($scope, $window, $http) {
-	$http({
-        method: "GET",
-        url: "php/front-end_endpoint.php",
-        params:{
-        	data:'categories'
-        }
-    }).success(function(response){
-        $scope.landCategories = [];
-        for (var category in response.categories) {
-            response.categories[category].apps = $scope.toArray(response.categories[category].apps);
-        	$scope.landCategories.push(response.categories[category]);
-
-            for(app in response.categories[category].apps){
-                response.categories[category].apps[app].links.urls = $scope.toArray(response.categories[category].apps[app].links.urls);
-                response.categories[category].apps[app].priority = parseInt(response.categories[category].apps[app].priority);
+    angular.element(document).ready(function () {
+        $http({
+            method: "GET",
+            url: "php/front-end_endpoint.php",
+            params:{
+                data:'categories'
             }
+        }).success(function(response){
+            $scope.landCategories = [];
+            for (var category in response.categories) {
+                response.categories[category].apps = $scope.toArray(response.categories[category].apps);
+                $scope.landCategories.push(response.categories[category]);
+
+                for(app in response.categories[category].apps){
+                    response.categories[category].apps[app].links.urls = $scope.toArray(response.categories[category].apps[app].links.urls);
+                    response.categories[category].apps[app].priority = parseInt(response.categories[category].apps[app].priority);
+                }
+            }
+        });
+
+        $scope.launch = function (url) {
+          var win = window.open(url, '_blank');
+          win.focus();
+        }
+
+        $scope.toArray = function (object) {
+            var elementsArray = [];
+            for(element in object){
+                elementsArray.push(object[element]);
+            }
+
+            return elementsArray;
         }
     });
-
-    $scope.launch = function (url) {
-      var win = window.open(url, '_blank');
-      win.focus();
-    }
-
-    $scope.toArray = function (object) {
-        var elementsArray = [];
-        for(element in object){
-            elementsArray.push(object[element]);
-        }
-
-        return elementsArray;
-    }
 }]);
